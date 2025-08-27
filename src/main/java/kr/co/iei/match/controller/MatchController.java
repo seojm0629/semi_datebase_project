3f6cd09c07a6141c3bfb1de50a40b36890290f5c
zerorhyme <simba6632@naver.com>
2025-08-27 13:44:52 +0900

    commit annotation

---
+++ src/main/java/kr/co/iei/match/controller/MatchController.java
@@ -0,0 +1,11 @@
+package kr.co.iei.match.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/match")
+public class MatchController {
+		
+	
+}
---
+++ src/main/java/kr/co/iei/match/model/dao/MatchDao.java
@@ -0,0 +1,8 @@
+package kr.co.iei.match.model.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MatchDao {
+	
+}

7ff91ea13df0e19f28cd7b18b1b4e56c087aeb3a
moonboyun <boyun805@gmail.com>
2025-08-27 20:51:39 +0900

    join commit

--- src/main/java/kr/co/iei/model/member/service/MemberService.java
+++ src/main/java/kr/co/iei/model/member/service/MemberService.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.iei.model.member.dao.MemberDao;
+import kr.co.iei.model.member.vo.Member;
 
 @Service
 public class MemberService {
 	
 	@Autowired
 	private MemberDao memberDao;
+
+	public int insertMember(Member m) {
+		int result = memberDao.insertMember(m);
+		return result;
+	}
 }

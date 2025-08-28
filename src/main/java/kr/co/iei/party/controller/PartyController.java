42c684b28390f8a813d48fe6982e660a7e04e431
jwaa33 <sksslssn@gmail.com>
2025-08-28 13:12:57 +0900

    123213231231213123213213213123123

--- src/main/java/kr/co/iei/party/controller/PartyController.java
+++ src/main/java/kr/co/iei/party/controller/PartyController.java
@@ -1,8 +1,13 @@
 package kr.co.iei.party.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PartyController {
 
+	@GetMapping(value = "/party")
+	public String partyPage() {
+		return "party/party";
+	}
 }

4cb89d7d6568ec4adf48af4cc08418e649480048
fuckingspring <sksslssn@gmail.com>
2025-08-28 18:21:49 +0900

    cm ws

--- src/main/java/kr/co/iei/party/controller/PartyController.java
+++ src/main/java/kr/co/iei/party/controller/PartyController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PartyController {
 
 	@GetMapping(value = "/party")
-	public String partyPage() {
+	public String party() {
 		return "party/party";
+	}
+
+	@GetMapping(value = "/party/partyadmin")
+	public String partyadmin() {
+		return "party/partyadmin";
+	}
+	
+	@GetMapping(value ="/party/partyuser")
+	public String partyuser() {
+		return "party/partyuser";
 	}
 }

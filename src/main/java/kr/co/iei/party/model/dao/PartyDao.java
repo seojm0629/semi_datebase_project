fb87c68e98913b0989077da9d91e3d4424aeceb2
fuckingspring <sksslssn@gmail.com>
2025-08-29 14:12:02 +0900

    ws cm

--- src/main/java/kr/co/iei/party/model/dao/PartyDao.java
+++ src/main/java/kr/co/iei/party/model/dao/PartyDao.java
@@ -1,8 +1,14 @@
 package kr.co.iei.party.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.iei.party.model.vo.Party;
 
 @Mapper
 public interface PartyDao {
+
+	List<Party> getAllParties();
 
 }

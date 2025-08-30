bbd2a184b9ab38b52b9b574b0a0ccea99f43499f
seojm0629 <seojm0629@gmail.com>
2025-08-30 19:04:52 +0900

    coommmmmmit

--- src/main/java/kr/co/iei/review/model/service/ReviewService.java
+++ src/main/java/kr/co/iei/review/model/service/ReviewService.java
@@ -1,13 +1,78 @@
 package kr.co.iei.review.model.service;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.iei.review.model.dao.ReviewDao;
+import kr.co.iei.review.model.vo.Review;
 
 @Service
 public class ReviewService {
 	
 	@Autowired
 	ReviewDao reviewDao;
+
+	
+
+	public int selectPartyTotalCount() {
+		int partyTotalCount = reviewDao.selectPartyTotalCount();
+		return partyTotalCount;
+	}
+
+
+
+	public List selectPhotoList(int start, int amount) {
+		int end = start + amount - 1;	//한 줄당 나올 페이지의 수 (1~3)
+		HashMap<String, Object> param = new HashMap<String, Object>();
+		param.put("start", start);
+		param.put("end", end);
+		List photoList = reviewDao.selectPhotoList(param);
+		System.out.println(photoList);
+		return photoList;
+	}
+
+
+
+	public int insertReview(Review r) {
+		int result = reviewDao.insertReview(r);
+		return result;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

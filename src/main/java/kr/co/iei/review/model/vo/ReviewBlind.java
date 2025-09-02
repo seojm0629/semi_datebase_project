f71380f0fed039afc5c4639eac53584084fbdd95
seojm0629 <seojm0629@gmail.com>
2025-09-02 15:15:49 +0900

    reviewCommit

---
+++ src/main/java/kr/co/iei/review/model/vo/ReviewBlind.java
@@ -0,0 +1,20 @@
+package kr.co.iei.review.model.vo;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Alias(value="reviewBlind")
+public class ReviewBlind {
+	private int reviewBlindNo;
+	private String reviewBlindWriter;
+	private String reviewBlindTitle;
+	private String reviewBlindContent;
+	private String reviewBlindImg;
+	private String reviewBlindJob;
+}

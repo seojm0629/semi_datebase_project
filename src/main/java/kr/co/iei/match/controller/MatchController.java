c72f2cc6dae541da6d28887deb29cd54bc062c05
moonboyun <boyun805@gmail.com>
2025-09-04 14:16:10 +0900

    fail commit

--- src/main/java/kr/co/iei/match/controller/MatchController.java
+++ src/main/java/kr/co/iei/match/controller/MatchController.java
@@ -71,7 +71,7 @@ public class MatchController {
 	
 	@GetMapping(value="/matchWrite")
 	public String matchWrite() {
-		return "/match/matchWrite";
+		return "match/matchWrite";
 	}
 	
 	@GetMapping(value="/enroll")
@@ -92,7 +92,7 @@ public class MatchController {
 				model.addAttribute("title", "신청 실패");
 				model.addAttribute("text", "신청에 실패했습니다. 다시 시도해주세요.");
 				model.addAttribute("icon", "error");
-				model.addAttribute("loc", "match/matchWrite");
+				model.addAttribute("loc", "/match/matchWrite");
 				
 				return "common/msg";
 			}
@@ -100,7 +100,7 @@ public class MatchController {
 		model.addAttribute("title", "신청 실패");
 		model.addAttribute("text", "신청에 실패했습니다. 다시 시도해주세요.");
 		model.addAttribute("icon", "error");
-		model.addAttribute("loc", "match/matchWrite");
+		model.addAttribute("loc", "/match/matchWrite");
 		
 		return "common/msg";
 	}

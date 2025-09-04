8331f42acbdf3d4626af7eb778f55fc74d96db25
fuckingspring <sksslssn@gmail.com>
2025-09-04 15:41:01 +0900

    ㅎㅇㅎㅇㅎㅇㅎㅇ

--- src/main/java/kr/co/iei/WebConfig.java
+++ src/main/java/kr/co/iei/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//스프링부트 설정파일임을 나타내는 어노테이션
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
@@ -14,60 +13,35 @@ public class WebConfig implements WebMvcConfigurer {
 	@Value(value = "${file.root}")
 	private String root;
 
-	// 서버가 관리하는 자원에 대한 접근 설정
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		/*
-		 * registry .addResourceHandler("사용자측에서 입력하는 경로");
-		 * .addResourceLocations("실제 서버경로");
-		 */
-
-		// spring boot 기본 설정(addResourceHandlers를 구현하지 않으면 기본적으로 설정되어있는 세팅)
-		// addResourceHandlers를 직접구현하면 기본세팅도 사라지므로 기본세팅 추가
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/", "classpath:/static/");
-		// question, review 경로
-		registry.addResourceHandler("/photo/image/**").addResourceLocations("file:///" + root + "/question/");
-		registry.addResourceHandler("/photo/image/**").addResourceLocations("file:///" + root + "/review_party/");
-		registry.addResourceHandler("/photo/image/**").addResourceLocations("file:///" + root + "/review_blind/");
 
-		//spring boot 기본 설정(addResourceHandlers를 구현하지 않으면 기본적으로 설정되어있는 세팅)
-		//addResourceHandlers를 직접구현하면 기본세팅도 사라지므로 기본세팅 추가
-		registry
-			.addResourceHandler("/**")
-			.addResourceLocations("classpath:/templates/", "classpath:/static/");
-		//question, review 경로
-		registry
-			.addResourceHandler("/photo/image/**")
-			.addResourceLocations("file:///"+root+"/question/");
-		registry
-			.addResourceHandler("/photo/image/**")
-			.addResourceLocations("file:///"+root+"/review_party/");
-		registry
-			.addResourceHandler("/photo/image/**")
-			.addResourceLocations("file:///"+root+"/review_blind/");
-			
-		registry
-			.addResourceHandler("/question/editor/**")
-			.addResourceLocations("file:///" + root + "/question/editor/");
-		registry
-			.addResourceHandler("/review/editor/**")
-			.addResourceLocations("file:///" + root + "/review_party/editor/");
-		registry
-			.addResourceHandler("/review/editor/**")
-			.addResourceLocations("file:///" + root + "/review_blind/editor/");
-		//그 외 작성은 밑에서 해주세요.
+		registry.addResourceHandler("/photo/image/question/**").addResourceLocations("file:///" + root + "/question/");
 
+		registry.addResourceHandler("/photo/image/review_party/**")
+				.addResourceLocations("file:///" + root + "/review_party/");
 
-		registry.addResourceHandler("/photo/editor/**").addResourceLocations("file:///" + root + "/question/editor/");
-		registry.addResourceHandler("/photo/editor/**").addResourceLocations("file:///" + root + "/review/editor/");
-		// 그 외 작성은 밑에서 해주세요.
+		registry.addResourceHandler("/photo/image/review_blind/**")
+				.addResourceLocations("file:///" + root + "/review_blind/");
 
-		registry
-		.addResourceHandler("/partyImg/**")
-		.addResourceLocations("file:///C:/Temp/upload/party/");
+		registry.addResourceHandler("/question/editor/**")
+				.addResourceLocations("file:///" + root + "/question/editor/");
+
+		registry.addResourceHandler("/review/editor/review_party/**")
+				.addResourceLocations("file:///" + root + "/review_party/editor/");
+
+		registry.addResourceHandler("/review/editor/review_blind/**")
+				.addResourceLocations("file:///" + root + "/review_blind/editor/");
+
+		registry.addResourceHandler("/photo/editor/question/**")
+				.addResourceLocations("file:///" + root + "/question/editor/");
+
+		registry.addResourceHandler("/photo/editor/review/**")
+				.addResourceLocations("file:///" + root + "/review/editor/");
+
+		registry.addResourceHandler("/partyImg/**").addResourceLocations("file:///C:/Temp/upload/party/");
 
 		registry.addResourceHandler("/member/memberImg/**").addResourceLocations("file:///" + root + "/selfPhoto/");
-
 	}
-
 }
